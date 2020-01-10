f0d466a6f859d112f32c84fc652bc357bf5ef898
kosta <kosta@192.168.0.104>
2020-01-10 16:55:16 +0900

    adminOffLecture 등록 수정 삭제 폼 만듬

---
+++ src/main/java/project/web/mvc/controller/AdminOffLectureController.java
@@ -0,0 +1,41 @@
+package project.web.mvc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import project.web.mvc.domain.Academy;
+import project.web.mvc.domain.OffLecture;
+import project.web.mvc.service.OffLectureService;
+
+@Controller
+@RequestMapping("/admin/offLecture")
+public class AdminOffLectureController {
+	
+	@Autowired
+	private OffLectureService offLectureService;
+	
+	@RequestMapping("")
+	public ModelAndView main() {
+		List<OffLecture> list = offLectureService.selectAll();
+		System.out.println(list);
+		return new ModelAndView("admin/adminOffLecture", "list", list);
+	}
+	
+	@RequestMapping("/adminOffLectureRegister")
+	public String register() {
+		//System.out.println("나와?");
+		return "admin/adminOffLectureRegister";
+	}
+	
+	@RequestMapping("/adminOffLectureRegister/insert")
+	public ModelAndView offLectureInsert(OffLecture offLecture) {
+		offLectureService.offLecInsert(offLecture);
+		List<OffLecture> list = offLectureService.selectAll();
+		System.out.println(list);
+		return new ModelAndView("admin/adminOffLecture", "list", list);
+	}
+}
